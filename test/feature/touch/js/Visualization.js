623859ecdf007c03b672398625de0a1774889338
Aaron Gloege <agloege@nerdery.com>
2012-11-21 22:48:10 -0600

    Add touch support test bed. Only iOS has been tested.

---
+++ test/feature/touch/js/Visualization.js
@@ -0,0 +1,87 @@
+define([
+    'layer/RunLoop',
+    'Box',
+    'layer/Scene'
+], function(
+    RunLoop,
+    Box,
+    Scene
+) {
+    'use strict';
+
+    var WIDTH = 250;
+    var HEIGHT = 250;
+    var BOX_WIDTH = 100;
+    var BOX_HEIGHT = 100;
+
+    var Visualization = function(wrapper) {
+        if (wrapper !== undefined) {
+            this.init(wrapper);
+        }
+    };
+
+    Visualization.prototype.init = function(wrapper) {
+
+        this.loop = new RunLoop();
+        
+        this.scene = new Scene(wrapper, WIDTH, HEIGHT);
+
+        this.box = new Box(
+            (WIDTH - BOX_WIDTH) * Math.random(),
+            (HEIGHT - BOX_HEIGHT) * Math.random(),
+            BOX_WIDTH,
+            BOX_HEIGHT
+        );
+
+        this.box.setSceneSize(WIDTH, HEIGHT);
+
+        this.scene.addChildToLayerByIndex(this.box, 0);
+        
+        return this.setupHandlers().enable();
+    };
+
+    Visualization.prototype.setupHandlers = function() {
+        this.updateCycle = this.update.bind(this);
+        this.renderCycle = this.render.bind(this);
+
+        return this;
+    };
+
+    Visualization.prototype.enable = function() {
+        this.loop
+            .addCall(this.updateCycle, RunLoop.UPDATE_CYCLE)
+            .addCall(this.renderCycle, RunLoop.RENDER_CYCLE);
+
+        return this;
+    };
+
+    Visualization.prototype.disable = function() {
+        this.loop
+            .removeCall(this.updateCycle, RunLoop.UPDATE_CYCLE)
+            .removeCall(this.renderCycle, RunLoop.RENDER_CYCLE);
+
+        return this;
+    };
+    
+    Visualization.prototype.start = function() {
+        this.loop.start();
+
+        return this;
+    };
+    
+    Visualization.prototype.stop = function() {
+        this.loop.stop();
+
+        return this
+    };
+
+    Visualization.prototype.update = function(elaapsed) {
+
+    };
+    
+    Visualization.prototype.render = function() {
+        this.scene.render();
+    };
+    
+    return Visualization;
+});
